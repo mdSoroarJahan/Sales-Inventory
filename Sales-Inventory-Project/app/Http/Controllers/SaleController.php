ddd6a518febe1ae5fbc299ba8c28e47c232bfbfb
mdSoroarJahan <soroarjahan17@gmail.com>
2025-12-04 02:28:51 +0600

    Added Create sale

---
+++ Sales-Inventory-Project/app/Http/Controllers/SaleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SaleController extends Controller
+{
+    public function salePage()
+    {
+        return view('pages.dashboard.sale-page');
+    }
+}
--- Sales-Inventory-Project/routes/web.php
+++ Sales-Inventory-Project/routes/web.php
@@ -6,9 +6,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SaleController;
 
 Route::get('/', [HomeController::class, 'homePage']);
 Route::get('/dashboard', [DashboardController::class, 'dashboardPage']);
 Route::get('/categoryPage', [CategoryController::class, 'categoryPage']);
 Route::get('/customerPage', [CustomerController::class, 'customerPage']);
 Route::get('/productPage', [ProductController::class, 'productPage']);
+Route::get('/salePage', [SaleController::class, 'salePage']);
