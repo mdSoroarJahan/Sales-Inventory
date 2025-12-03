7f40f6aafd3b8122622b839d1c328d5521b5b719
mdSoroarJahan <soroarjahan17@gmail.com>
2025-12-04 02:19:50 +0600

    Added Product

--- Sales-Inventory-Project/routes/web.php
+++ Sales-Inventory-Project/routes/web.php
@@ -5,9 +5,10 @@ use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DashboardController;
-
+use App\Http\Controllers\ProductController;
 
 Route::get('/', [HomeController::class, 'homePage']);
 Route::get('/dashboard', [DashboardController::class, 'dashboardPage']);
 Route::get('/categoryPage', [CategoryController::class, 'categoryPage']);
 Route::get('/customerPage', [CustomerController::class, 'customerPage']);
+Route::get('/productPage', [ProductController::class, 'productPage']);
