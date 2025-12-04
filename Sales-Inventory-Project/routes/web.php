fc5da0ca55f05ed79610d49ddabc78f87e873ea9
mdSoroarJahan <soroarjahan17@gmail.com>
2025-12-04 15:46:57 +0600

    Added Regestration page frontend

--- Sales-Inventory-Project/routes/web.php
+++ Sales-Inventory-Project/routes/web.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\SaleController;
+use App\Http\Controllers\UserController;
+use Symfony\Component\HttpKernel\DependencyInjection\RegisterControllerArgumentLocatorsPass;
 
 Route::get('/', [HomeController::class, 'homePage']);
 Route::get('/dashboard', [DashboardController::class, 'dashboardPage']);
@@ -18,3 +20,4 @@ Route::get('/productPage', [ProductController::class, 'productPage']);
 Route::get('/salePage', [SaleController::class, 'salePage']);
 Route::get('/invoicePage', [InvoiceController::class, 'invoicePage']);
 Route::get('/reportPage', [ReportController::class, 'reportPage']);
+Route::get('/registrationPage', [UserController::class, 'registrationPage']);
