46b3c5c0c19dbb841c628f7400f96fdc3034f5dd
mdSoroarJahan <soroarjahan17@gmail.com>
2026-01-24 22:15:33 +0600

    Implement customer creation functionality; add customerCreate method in CustomerController with validation and error handling

--- Sales-Inventory-Project/app/Http/Controllers/CustomerController.php
+++ Sales-Inventory-Project/app/Http/Controllers/CustomerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
@@ -9,5 +10,28 @@ class CustomerController extends Controller
     public function customerPage()
     {
         return view('pages.dashboard.customer-page');
+    }
+
+    public function customerCreate(Request $request)
+    {
+        $user_id = $request->header('user_id');
+        $request->validate([
+            'name' => 'required|50',
+            'email' => 'required|unique',
+            'mobile' => 'required|50'
+        ]);
+
+        try {
+            $customer = Customer::create([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'mobile' => $request->input('mobile')
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Failed to create user'
+            ]);
+        }
     }
 }
